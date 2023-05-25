d5c5ffdaea5ef074d0590fc33537ccc71236b15d
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 15:41:37 -0300

    curso java

--- src/Main6.java
+++ src/Main6.java
@@ -1,4 +1,5 @@
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.time.format.TextStyle;
 import java.util.Locale;
 
@@ -10,6 +11,21 @@ public class Main6 {
         System.out.println("Olá, "+nome+" hoje é " +hoje+" bom dia.");
         Locale brasil = new Locale("pt","br");
         System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
+        String dia = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        String saudacao;
+        LocalDateTime agora = LocalDateTime.now();
+        System.out.println(agora);
+
+        if(agora.getHour() >= 0 && agora.getHour() < 12){
+
+            saudacao = "bom dia";
+        }else if(agora.getHour() >= 12 && agora.getHour() < 18){
+            saudacao = "boa tarde";
+        }else{
+            saudacao = "boa noite";
+        }
+
+        System.out.printf("olá, %s. hoje é %s, %s.", nome, dia, saudacao.toLowerCase());
 
     }
 }
