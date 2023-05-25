eb72755660f9e548c16fb9edf304582c39616401
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 15:50:39 -0300

    aula7

---
+++ src/Main7.java
@@ -0,0 +1,20 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Main7 {
+    public static void main(String[] args) {
+
+      //laços numericos
+
+        for(int c=1; c<=10; c++){
+
+            System.out.println("vai dar certo"+ c);
+
+
+
+        }
+
+    }
+}
