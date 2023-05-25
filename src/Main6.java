5931dcfe002a18bbd6c57751e8a49d0b60fc6ad9
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 15:29:25 -0300

    atu

--- src/Main6.java
+++ src/Main6.java
@@ -1,4 +1,6 @@
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
 
 public class Main6 {
     public static void main(String[] args) {
@@ -6,6 +8,8 @@ public class Main6 {
        String nome = "Ana";
         LocalDate hoje = LocalDate.now();
         System.out.println("Olá, "+nome+" hoje é " +hoje+" bom dia.");
+        Locale brasil = new Locale("pt","br");
+        System.out.println(hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil));
 
     }
 }
