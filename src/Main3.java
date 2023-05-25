a7e542955535f18d633dcf8b520f2850b5dc8311
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 14:47:34 -0300

    atu

---
+++ src/Main3.java
@@ -0,0 +1,17 @@
+
+public class Main3 {
+    public static void main(String[] args) {
+int nota = 50;
+//nota>=70 aprovado se não reprovado
+if (nota>=70) {
+
+    System.out.println("aluno aprovado!");
+    
+} else {
+    
+    System.out.println("aluno reprovado!");
+}
+
+
+    }
+}
---
+++ src/Main4.java
@@ -0,0 +1,37 @@
+
+public class Main4 {
+    public static void main(String[] args) {
+        String grad = "G";
+        int nota = 0;
+if (nota>= 80) {
+         grad = "A";
+    System.out.println("A");
+    
+} else if(nota>=70){
+     grad = "B";
+    System.out.println("B");
+    
+}else if (nota>=60) {
+     grad = "C";
+    System.out.println("c");
+}else {
+     grad = "D";
+System.out.println("d");    
+}
+
+switch (grad) {
+    case "A":
+    case "B":
+    case "C":
+
+    System.out.println("aprovado");
+        
+        break;
+
+        case "D":
+        System.out.println("reprovado");
+    default:
+        break;
+}
+    }
+}
