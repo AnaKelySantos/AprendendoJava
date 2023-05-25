6d98e8e1a82f11809e46add69014175edaa8182e
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 13:47:48 -0300

    operadores

--- src/Main.java
+++ src/Main.java
@@ -6,5 +6,18 @@ public class Main {
         String nome = "AnaK";
         System.out.println("olá, "+ nome);
 
+        int a = 1;
+        int b = 2;
+         
+        int soma= a+b;
+        System.out.println(soma);
+        int menos = a-b;
+        int vezes = a*b;
+        int dividir= a / b;
+        int mult = b*=a;
+        System.out.println(mult);
+        System.out.println(vezes);
+        System.out.println(dividir);
+        System.out.println(menos);
     }
 }
