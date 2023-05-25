aca6e9c562ece56c9212b1386fe19abca9f80801
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 15:18:26 -0300

    atu

---
+++ src/Main6.java
@@ -0,0 +1,11 @@
+import java.time.LocalDate;
+
+public class Main6 {
+    public static void main(String[] args) {
+
+       String nome = "Ana";
+        LocalDate hoje = LocalDate.now();
+        System.out.println("Olá, "+nome+" hoje é " +hoje+" bom dia.");
+
+    }
+}
