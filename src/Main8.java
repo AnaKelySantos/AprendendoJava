01476bb12b062efad3b32d6daae84bb71c73ee41
AnaKelySantos <anasa.prof@gmail.com>
2023-05-25 16:14:17 -0300

    aula8

---
+++ src/Main8.java
@@ -0,0 +1,25 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Main8 {
+    public static void main(String[] args) {
+
+      //laços numericos
+
+        for(int c=1; c<=10; c++){
+            
+            for(int i=1; i<=10; i++){
+                
+                      int f =  c * i;
+                System.out.println(i +"x"+c+"="+ f);
+            
+
+
+            }
+
+        }
+
+    }
+}
