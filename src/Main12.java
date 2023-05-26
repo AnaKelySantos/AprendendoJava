5a040ec5ab68964a52451cd23c06dca38c0f4282
AnaKelySantos <anasa.prof@gmail.com>
2023-05-26 10:39:15 -0300

    aula12

---
+++ src/Main12.java
@@ -0,0 +1,16 @@
+
+
+public class Main12 {
+    public static void main(String[] args) {
+
+      int resultado = soma(2, 3);
+
+      System.out.println(resultado);
+      
+    }
+
+    public static int soma(int a,int b){
+      return a+b;
+    }
+
+}
