a006541709011b9b9956a9966f7f60a0517f6a89
AnaKelySantos <anasa.prof@gmail.com>
2023-05-26 10:00:47 -0300

    atu

--- src/Main9.java
+++ src/Main9.java
@@ -1,19 +1,26 @@
-import java.time.LocalDate;
-import java.time.LocalDateTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
+import java.util.Arrays;
 
 public class Main9 {
     public static void main(String[] args) {
 
       //vetores
       
-      int[] numeros = new int[2];
-      numeros[0] = 1;
-      numeros[1] = 2;
+      String[] letras = new String[4];
+      letras[0] = "a";
+      letras[1] = "b";
+      letras[2] = "c";
+      letras[3] = "d";
 
-      System.out.println(numeros);
-      
+      for(int i = 0; i < letras.length; i++){
+        System.out.println(letras[i]);
+      }
+
+      String teste[] = {"f","j","k","l","m"};
+      for(int i = 0; i < teste.length; i++){
+        System.out.println(teste[i]);
+      }
+
+      System.out.println(Arrays.toString(letras));
 
     }
 }
