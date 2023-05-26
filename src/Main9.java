63fde84e6cd1cf0c23c5043e64990203a654c039
AnaKelySantos <anasa.prof@gmail.com>
2023-05-26 09:43:06 -0300

    atu

---
+++ src/Main9.java
@@ -0,0 +1,19 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
+public class Main9 {
+    public static void main(String[] args) {
+
+      //vetores
+      
+      int[] numeros = new int[2];
+      numeros[0] = 1;
+      numeros[1] = 2;
+
+      System.out.println(numeros);
+      
+
+    }
+}
