246001d41a4d3f339fd7322e841b1719c746f288
AnaKelySantos <anasa.prof@gmail.com>
2023-05-26 10:29:52 -0300

    atu

---
+++ src/Main11.java
@@ -0,0 +1,17 @@
+import java.util.Arrays;
+
+public class Main11 {
+    public static void main(String[] args) {
+
+      String nomek = "kely";
+      saudacao(nomek);
+
+    }
+
+    //função
+    public static void saudacao( String nomen){
+
+      System.out.println("hello, " + nomen);
+    }    
+
+}
